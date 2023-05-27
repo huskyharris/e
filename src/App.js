28925c228f08d1462925ba4973e15eb374ed4ec2
esosa <esosaosaigbovo@gmail.com>
2023-05-27 16:09:52 +0200

    AA

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="maintenance-page">
+      <h1>Website Under Maintenance</h1>
+      <p>We apologize for the inconvenience. Our website is currently undergoing scheduled maintenance. Please check back later.</p>
+    </div>
+  );
+};
+
+
+export default App;
